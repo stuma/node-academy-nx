a3bb7767927fd13116c27a828a50794feeef3d22
Sebastián Tuma <seba.tuma@gmail.com>
2022-02-03 23:13:38 -0300

    Tarea 10 - Arreglando la salida de nuestro programa

--- utils.js
+++ utils.js
@@ -9,7 +9,7 @@ const createTable = async (base, listar) => {
     for(var i = 1; i <= 10; i++) {
         content = `${content}${base} x ${i} = ${base*i} \n`
     }
-    fs.writeFile('output.txt', content, err => {
+    fs.writeFile('./salida/output.txt', content, err => {
         if (err) {
             console.error(err.red)
             return
