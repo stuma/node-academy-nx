30985a976604c9f102f6c97517dc756f936c0593
Sebastián Tuma <seba.tuma@gmail.com>
2022-02-03 21:27:10 -0300

    Tarea 9 - Colores de la consola

--- utils.js
+++ utils.js
@@ -3,6 +3,9 @@ var colors = require('colors')
 
 const createTable = async (base, listar) => {
     let content = ''
+    content = `${content}=================== \n`
+    content = `${content}    Tabla del: ${base}     \n`
+    content = `${content}=================== \n`
     for(var i = 1; i <= 10; i++) {
         content = `${content}${base} x ${i} = ${base*i} \n`
     }
